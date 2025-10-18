d1c5ce781d1cf6b3d50bae494fe00ebd8006b977
R.Thenmozhi <thenmozhir2818@gmail.com>
2025-10-18 12:29:47 +0530

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,17 @@
+//Declaring variables 
+
+let name = "Thenmozhi"; //String
+let age = 23; //Number
+let isDeveloper = true; // Boolean
+
+//Printing values in console 
+
+console.log("Name:", name);
+console.log("Age", age);
+console.log("Is Developer?", isDeveloper);
+
+//checking type of each variable
+
+console.log(typeof name);  //Output: string
+console.log(typeof age); //Output: number 
+console.log(typeof isDeveloper); //Output: boolean
